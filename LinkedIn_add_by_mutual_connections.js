f6b12536fc8f448b840375ebd590e391996e90ea
XMMR12 <31443996+XMMR12@users.noreply.github.com>
2021-04-02 22:19:25 +0200

    The Basic LinkedIn add by mutual connections
    [Small fixes]
    
    The Script has no filtration by jobs yet!
    
    i will add it so soon

--- LinkedIn_add_by_mutual_connections.js
+++ LinkedIn_add_by_mutual_connections.js
@@ -2,12 +2,13 @@
 //https://www.linkedin.com/mynetwork/
 //**Desktop Site**
 //basic model:
+function build_network(){
 var allbts=document.getElementsByClassName("full-width artdeco-button artdeco-button--2 artdeco-button--full artdeco-button--secondary ember-view");
-var num_mf=prompt("Enter the Number of Mutual friends you want in a connection:\n[[0 for Adding everyone]");
+var num_mf=parseInt(prompt("Enter the Number of Mutual friends you want in a connection:\n[[0 for Adding everyone]"));
 for (var i=0;i<allbts.length-1;i++){
 if (allbts[i].textContent.toLowerCase().search("connect")!=-1)
 {
-if (num_mf=="0"){
+if (num_mf==0){
 allbts[i].click();
 }
 else if(allbts[i].parentElement.parentElement.children[0].textContent.search("mutual connections")!=-1){
@@ -17,3 +18,5 @@ if (mut_cons>=num_mf)
 }
 }
 }
+}
+build_network();
